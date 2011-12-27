5bf119b9c7ab852da7d5b35bb0c8a090653a2eed
lixiaojun <cn.lixiaojun@gmail.com>
2011-12-28 00:25:11 +0800

    test mysql db

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ Created on 2011-10-8
 '''
 from jobs.CyeFilterInc import JingdongFilter
 from jobs.models import cyeTbReflector, CyeTbRow
-from libs.cyetools import cyeDBpoool
+from libs.cyetools import cyeDBpoool, testCyecursor
 from twisted.enterprise import reflector
 import random
 import time
@@ -21,25 +21,43 @@ def gotCye(datas):
         print "Got:", data.id
         
 def getCyeData():
-    return cyeDBpoool.runQuery("SELECT * FROM cye_tb")
+    return 
 
 def printResult(l):
+    print 'printResult'
     if l:
         print l
     else:
         print "No such data"
-    
+        
+def _insert(tx, item):
+    tx.exectue("select * from cye_db")
+    result = tx.fetchone()
+    print result
+    if result:
+        print result
+        
+def handle_error(e):
+        print e
+        
+def getData():
+    print 'getData'
+    query = cyeDBpoool.runInteraction(_insert)
+    query.addErrback(handle_error)
+    return query
+        
 if __name__ == '__main__':
-    
+    '''
     fp = open('detail.dat')
     html = fp.read()
     fp.close()
     
     detail = JingdongFilter.handleDetail(html)
-    
+    '''
     #print detail
     
     newItem = CyeTbRow()
+    newItem.assignKeyAttr('id', '')
     newItem.key = 'test'+str(random.randint(1, 999))
     newItem.title = 'test'
     newItem.url = 'url'
@@ -48,13 +66,19 @@ if __name__ == '__main__':
     newItem.detail = 'detail'
     newItem.utime = time.strftime('%Y-%m-%d %X', time.localtime())
     
-    #cyeTbReflector.insertRow(newItem).addCallback(onInsert)
+    cyeTbReflector.insertRow(newItem).addCallback(onInsert)
     
     d = cyeTbReflector.loadObjectsFrom("cye_tb")
     
     d.addCallback(gotCye)
     print 'hello'
     
-    getCyeData().addCallback(printResult)
+    cyeDBpoool.runQuery("SELECT * FROM cye_tb").addCallback(printResult)
+    
+    
+    count = testCyecursor.execute('SELECT * FROM cye_tb')
+    print count
+    
+    getData().addBoth(printResult)
     
     pass
